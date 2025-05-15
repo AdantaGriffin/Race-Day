a2724ef4ac19a91ebbb34243b55b16048589474c
Adanta Griffin <griffinadanta90@gmail.com>
2025-05-15 10:14:55 -0400

    updated html css js

--- raceDay.js
+++ raceDay.js
@@ -1,20 +1,25 @@
-let raceNumber = Math.floor(Math.random() * 1000); 
-let early = false;
-let age = 18;
+submit.addEventListener('click', () => {
 
-if (age >= 18 && early) {
-  raceNumber = raceNumber + 1000;
-  //console.log(raceNumber)
-}
- 
-if (age > 18 && early) {
-  console.log(`Your race number ${raceNumber} start\'s at 9:30`);
-} else if(age > 18 && !early) {
-  console.log(`Your race number ${raceNumber} start\'s at 11:00`);
-}
-if (age < 18) {
-  console.log(`Your race number ${raceNumber} start\'s at 12:30`);
-} else {
-  console.log(`Please see registration desk`);
-}
+  let raceNumber = Math.floor(Math.random() * 1000); 
+  let early = true;
+  let ageInput = document.getElementById('age-input');
 
+  let age = ageInput.value;
+  let raceDisplay = document.getElementById('race');
+
+  if (age >= 18 && early) {
+    raceNumber = raceNumber + 1000;
+    //console.log(raceNumber)
+  }
+  
+  if (age > 18 && early) {
+    raceDisplay.innerText = `Your race number ${raceNumber} start\'s at 9:30`;
+  } else if(age > 18 && !early) {
+    raceDisplay.innerText = `Your race number ${raceNumber} start\'s at 11:00`;
+  }
+  else if (age < 18) {
+    raceDisplay.innerText = `Your race number ${raceNumber} start\'s at 12:30`;
+  } else {
+    raceDisplay.innerText = `Please see registration desk`;
+  }
+});
